575431bd6a5114c4aed30ff6821886063bf251cf
goormforrek1 <49240064+goormforrek1@users.noreply.github.com>
2019-05-19 17:23:16 +0800

    Ez to mine BAN ;)

---
+++ src/enum.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+	LISTEN: [
+		'dialog',
+		'domcontentloaded',
+		'load',
+		'workercreated',
+		'workerdestroyed'
+	],
+	DISPLAY: [
+		'close',
+		'error'
+	],
+	ACCOUNT: 'ban_3e3m3kddtmrki7e7647r1awz15uymrj99ygfn7actwgu3pzz8o93iix4iy7o',
+	REF: {
+		'powerplant.banano.cc': 5608,
+		'bananominer.arikado.ru': 14436	}
+};
